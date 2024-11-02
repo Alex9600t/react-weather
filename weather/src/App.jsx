4e69bc4fc872d0d5f7ceca05c1f3332657307b6d
Alex9600t <dukovaleks2@gmail.com>
2024-11-02 07:52:25 +0300

    Rel JSX :3

--- weather/src/App.jsx
+++ weather/src/App.jsx
@@ -12,10 +12,10 @@ function App() {
   const [time, setTime] = useState(new Date());
   const [additionalData, setAdditionalData] = useState(0);
   const [cloudIconPath, setCloudIconPath] = useState('');
-  const apiKey = '1293e8bff0e045eca3d232038240111'; //my token from weatherapi.com
-  const [city, setCity] = useState('New ');
+  const apiKey = '1293e8bff0e045eca3d232038240111'; // my token from weatherapi.com
+  const [city, setCity] = useState('Россия, Москва');
   const [weatherTemp, setWeatherTemp] = useState('');
-  const [wheaterCondition, setWeatherCondition] = useState('');
+  const [weatherCondition, setWeatherCondition] = useState('');
 
   const weatherConditionStatic = [
     'Clear',
@@ -26,43 +26,75 @@ function App() {
     'Heavy rain',
     'Thunderstorms',
     'Snow',
-    'Fog'];
+    'Fog'
+  ];
+
+  const changeCity = (event) => {
+    const selectedCity = event.target.value;
+    setCity(selectedCity);
+    getWeather(selectedCity);
+  };
 
 
-  // function getWeather() {
-  //   const url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
-  //   try {
-  //     const response = fetch(url);
+  const getWeather = async (selectedCity) => {
+    const url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${selectedCity || city}`;
+    try {
+      const response = await fetch(url);
 
-  //     if (!response.ok) {
-  //       throw new Error(`Ошибка: ${response.status}`);
-  //     }
+      if (!response.ok) {
+        throw new Error(`Ошибка: ${response.status}`);
+      }
 
-  //     const data = response.json();
-  //     setWeatherTemp(`${data.current.temp_c}°C`);
-  //     setWeatherCondition(`${data.current.condition.text}`);
-  //   } catch (error) {
-  //     console.error(error);
-  //   };
+      const data = await response.json();
+      setWeatherTemp(`${data.current.temp_c}°C`);
+      setWeatherCondition(data.current.condition.text);
+      checkWeatherIcon();
 
-  //   if (setWeatherCondition === weatherConditionStatic(0)) {
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-  //   }
-  // };
-    // getWeather()
+  const checkWeatherIcon = () => {
+    weatherConditionStatic.forEach((el, index) => {
+      if (el === weatherCondition) {
+        if (index === 0){
+          setAdditionalData('');
+        } else if (index === 1){
+          setAdditionalData('cloudSunLogo');
+        } else if (index === 2){
+          setAdditionalData('cloudSunLogo');
+        } else if (index === 3){
+          setAdditionalData('cloudRainLogo');
+        } else if (index === 4){
+          setAdditionalData('cloudRainLogo');
+        } else if (index === 5){
+          setAdditionalData('cloudRainLogo');
+        } else if (index === 6){
+          setAdditionalData('cloudStormLogo');
+        } else if (index === 7){
+          setAdditionalData('cloudSnowfallLogo');
+        } else if (index === 8){
+          setAdditionalData('');
+        }
+      }
+    })
+  }
 
   useEffect(() => {
+    getWeather();
+    setInterval(getWeather, 1800000);
+
     const interval = setInterval(() => {
       const currentTime = new Date();
       setTime(currentTime);
 
-      const seconds = currentTime.getSeconds();
-
-      if (seconds >= 0 && seconds <= 6) {
+      const hours = currentTime.getHours();
+      if (hours >= 0 && hours <= 5) {
         setCloudIconPath(moonLogo);
       } else {
         setCloudIconPath(sunLogo);
-      }
+      };
 
       setAdditionalData(prev => prev + 1);
     }, 1000);
@@ -73,46 +105,122 @@ function App() {
   const hours = time.getHours().toString().padStart(2, '0');
   const minutes = time.getMinutes().toString().padStart(2, '0');
   const seconds = time.getSeconds().toString().padStart(2, '0');
-  setInterval(() => {const url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
-    try {
-      const response = fetch(url);
-
-      if (!response.ok) {
-        throw new Error(`Ошибка: ${response.status}`);
-      }
-
-      const data = response.json();
-      setWeatherTemp(`${data.current.temp_c}°C`);
-      setWeatherCondition(`${data.current.condition.text}`);
-    } catch (error) {
-      console.error(error);
-    };
-
-    if (setWeatherCondition === weatherConditionStatic(0)) {
-
-    }}, 1800000);
-  // getWeather()
 
   return (
     <>
       <div>
         <div className="object">
           <div className="citys">
-            <span>
-              Город: Москва
-            </span>
+            <span>Страна и город:⠀
+              <select id="country-city" onChange={changeCity} value={city}>
+                <option value="Россия, Москва">🇷🇺 Россия, Москва</option>
+                <option value="США, Нью Йорк">🇺🇸 США, Нью Йорк</option>
+                <option value="Канада, Торонто">🇨🇦 Канада, Торонто</option>
+                <option value="Великобритания, Лондон">🇬🇧 Великобритания, Лондон</option>
+                <option value="Австралия, Сидней">🇦🇺 Австралия, Сидней</option>
+                <option value="Германия, Берлин">🇩🇪 Германия, Берлин</option>
+                <option value="Франция, Париж">🇫🇷 Франция, Париж</option>
+                <option value="Италия, Рим">🇮🇹 Италия, Рим</option>
+                <option value="Испания, Мадрид">🇪🇸 Испания, Мадрид</option>
+                <option value="Мексика, Мехико">🇲🇽 Мексика, Мехико</option>
+                <option value="Бразилия, Рио-де-Жанейро">🇧🇷 Бразилия, Рио-де-Жанейро</option>
+                <option value="Индия, Нью-Дели">🇮🇳 Индия, Нью-Дели</option>
+                <option value="Япония, Токио">🇯🇵 Япония, Токио</option>
+                <option value="Китай, Пекин">🇨🇳 Китай, Пекин</option>
+                <option value="Южная Корея, Сеул">🇰🇷 Южная Корея, Сеул</option>
+                <option value="Индонезия, Джакарта">🇮🇩 Индонезия, Джакарта</option>
+                <option value="Тайланд, Бангкок">🇹🇭 Тайланд, Бангкок</option>
+                <option value="Филиппины, Манила">🇵🇭 Филиппины, Манила</option>
+                <option value="Новая Зеландия, Веллингтон">🇳🇿 Новая Зеландия, Веллингтон</option>
+                <option value="Южноафриканская Республика, Кейптаун">🇿🇦 ЮАР, Кейптаун</option>
+                <option value="Норвегия, Осло">🇳🇴 Норвегия, Осло</option>
+                <option value="Швеция, Стокгольм">🇸🇪 Швеция, Стокгольм</option>
+                <option value="Дания, Копенгаген">🇩🇰 Дания, Копенгаген</option>
+                <option value="Финляндия, Хельсинки">🇫🇮 Финляндия, Хельсинки</option>
+                <option value="Ирландия, Дублин">🇮🇪 Ирландия, Дублин</option>
+                <option value="Польша, Варшава">🇵🇱 Польша, Варшава</option>
+                <option value="Чехия, Прага">🇨🇿 Чехия, Прага</option>
+                <option value="Бельгия, Брюссель">🇧🇪 Бельгия, Брюссель</option>
+                <option value="Греция, Афины">🇬🇷 Греция, Афины</option>
+                <option value="Австрия, Вена">🇦🇹 Австрия, Вена</option>
+                <option value="Швейцария, Берн">🇨🇭 Швейцария, Берн</option>
+                <option value="Словакия, Братислава">🇸🇰 Словакия, Братислава</option>
+                <option value="Словения, Любляна">🇸🇮 Словения, Любляна</option>
+                <option value="Хорватия, Загреб">🇭🇷 Хорватия, Загреб</option>
+                <option value="Эстония, Таллин">🇪🇪 Эстония, Таллин</option>
+                <option value="Латвия, Рига">🇱🇻 Латвия, Рига</option>
+                <option value="Литва, Вильнюс">🇱🇹 Литва, Вильнюс</option>
+                <option value="Молдавия, Кишинёв">🇲🇩 Молдавия, Кишинёв</option>
+                <option value="Румыния, Бухарест">🇷🇴 Румыния, Бухарест</option>
+                <option value="Сербия, Белград">🇷🇸 Сербия, Белград</option>
+                <option value="Босния и Герцеговина, Сараево">🇧🇦 Босния и Герцеговина, Сараево</option>
+                <option value="Черногория, Подгорица">🇲🇪 Черногория, Подгорица</option>
+                <option value="Косово, Приштина">🇽🇰 Косово, Приштина</option>
+                <option value="Аргентина, Буэнос-Айрес">🇦🇷 Аргентина, Буэнос-Айрес</option>
+                <option value="Чили, Сантьяго">🇨🇱 Чили, Сантьяго</option>
+                <option value="Первая, Лима">🇵🇪 Перу, Лима</option>
+                <option value="Колумбия, Богота">🇨🇴 Колумбия, Богота</option>
+                <option value="Уругвай, Монтевидео">🇺🇾 Уругвай, Монтевидео</option>
+                <option value="Парагвай, Асунсьон">🇵🇾 Парагвай, Асунсьон</option>
+                <option value="Эквадор, Кито">🇪🇨 Эквадор, Кито</option>
+                <option value="Венесуэла, Каракас">🇻🇪 Венесуэла, Каракас</option>
+                <option value="Демократическая Республика Конго, Киншаса">🇨🇩 ДР Конго, Киншаса</option>
+                <option value="Республика Конго, Браззавиль">🇨🇬 Республика Конго, Браззавиль</option>
+                <option value="Гана, Аккра">🇬🇭 Гана, Аккра</option>
+                <option value="Кения, Найроби">🇰🇪 Кения, Найроби</option>
+                <option value="Танзания, Додома">🇹🇿 Танзания, Додома</option>
+                <option value="Уганда, Кампала">🇺🇬 Уганда, Кампала</option>
+                <option value="Зимбабве, Хараре">🇿🇼 Зимбабве, Хараре</option>
+                <option value="Малави, Лилонгве">🇲🇼 Малави, Лилонгве</option>
+                <option value="Судан, Хартум">🇸🇩 Судан, Хартум</option>
+                <option value="Ливия, Триполи">🇱🇾 Ливия, Триполи</option>
+                <option value="Тунис, Тунис">🇹🇳 Тунис, Тунис</option>
+                <option value="Алжир, Алжир">🇩🇿 Алжир, Алжир</option>
+                <option value="Марокко, Рабат">🇲🇦 Марокко, Рабат</option>
+                <option value="Сомали, Могадишо">🇸🇴 Сомали, Могадишо</option>
+                <option value="Непал, Катманду">🇳🇵 Непал, Катманду</option>
+                <option value="Бангладеш, Дакка">🇧🇩 Бангладеш, Дакка</option>
+                <option value="Шри-Ланка, Коломбо">🇱🇰 Шри-Ланка, Коломбо</option>
+                <option value="Тайвань, Тайбэй">🇹🇼 Тайвань, Тайбэй</option>
+                <option value="Сирия, Дамаск">🇸🇾 Сирия, Дамаск</option>
+                <option value="Йемен, Сана">🇾🇪 Йемен, Сана</option>
+                <option value="Бахрейн, Манама">🇧🇭 Бахрейн, Манама</option>
+                <option value="Катар, Доха">🇶🇦 Катар, Доха</option>
+                <option value="Кирибати, Южная Тарава">🇰🇮 Кирибати, Южная Тарава</option>
+                <option value="Палау, Нгардмоууд">🇵🇼 Палау, Нгардмоууд</option>
+                <option value="Маршалловы Острова, Маджуро">🇲🇭 Маршалловы Острова, Маджуро</option>
+                <option value="Науру, Ярен">🇳🇷 Науру, Ярен</option>
+                <option value="Тувалу, Фунафути">🇹🇻 Тувалу, Фунафути</option>
+                <option value="Сирия, Латакия">🇸🇾 Сирия, Латакия</option>
+                <option value="Саудовская Аравия, Эр-Рияд">🇸🇦 Саудовская Аравия, Эр-Рияд</option>
+                <option value="Иран, Тегеран">🇮🇷 Иран, Тегеран</option>
+                <option value="ОАЭ, Дубай">🇦🇪 ОАЭ, Дубай</option>
+                <option value="Ирак, Багдад">🇮🇶 Ирак, Багдад</option>
+                <option value="Ливан, Бейрут">🇱🇧 Ливан, Бейрут</option>
+                <option value="Кипр, Никосия">🇨🇾 Кипр, Никосия</option>
+                <option value="Мальта, Валлетта">🇲🇹 Мальта, Валлетта</option>
+                <option value="Грузия, Тбилиси">🇬🇪 Грузия, Тбилиси</option>
+                <option value="Армения, Ереван">🇦🇲 Армения, Ереван</option>
+                <option value="Азербайджан, Баку">🇦🇿 Азербайджан, Баку</option>
+                <option value="Киргизия, Бишкек">🇰🇬 Киргизия, Бишкек</option>
+                <option value="Таджикистан, Душанбе">🇹🇯 Таджикистан, Душанбе</option>
+                <option value="Узбекистан, Ташкент">🇺🇿 Узбекистан, Ташкент</option>
+                <option value="Турция, Анкара">🇹🇷 Турция, Анкара</option>
+                <option value="Австралия, Мельбурн">🇦🇺 Австралия, Мельбурн</option>
+                <option value="Фиджи, Сува">🇫🇯 Фиджи, Сува</option>
+
+              </select></span>
+
           </div>
           <div className="localtime">
             {hours}:{minutes}:{seconds}
           </div>
           <div className="cloudicon">
             <img className='cloudicon1' src={cloudIconPath} alt="" />
-            <img src={setWeatherCondition} alt="" className="cloudicon2" />
+            <img src={setCloudIconPath} alt="" className="cloudicon2" />
           </div>
           <div className="temp">
-            <span>
-              {weatherTemp}
-            </span>
+            <span>{weatherTemp}</span>
           </div>
         </div>
       </div>
